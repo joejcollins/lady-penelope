97222bd9cf85afcdf0b59d7ce5b18aae210c9443
Joe J Collins <joejcollins@users.noreply.github.com>
2024-12-13 17:22:50 +0000

    Update post-create.sh

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,2 +1,9 @@
+# Add the required configuration to /etc/rstudio/rserver.conf
+echo "Configuring RStudio Server..."
+sudo bash -c 'cat << EOF >> /etc/rstudio/rserver.conf
+auth-none=1
+auth-minimum-user-id=1000
+EOF'
+
 # Make the virtual environment.
 make venv
