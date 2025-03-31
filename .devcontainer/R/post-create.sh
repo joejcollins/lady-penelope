7cbc3e7ca3adb7a7bf3c1e1a507372d765d6a3c4
Joe J Collins <joejcollins@users.noreply.github.com>
2025-03-31 10:32:11 +0000

    Simplified R server

--- .devcontainer/R/post-create.sh
+++ .devcontainer/R/post-create.sh
@@ -12,6 +12,14 @@ sudo sed -i 's/^log-level=.*$/log-level=info/' /etc/rstudio/logging.conf
 # Set the default workspaces
 echo "Setting default user for R sessions..."
 sudo bash -c 'cat << EOF > /etc/rstudio/rsession.conf
-session-default-working-dir=/workspaces/lady-penelope
-session-default-new-project-dir=/workspaces/lady-penelope
+session-default-working-dir=/workspaces/$RepositoryName
+session-default-new-project-dir=/workspaces/$RepositoryName
 EOF'
+
+# Restart the rserver with sudo otherwise it won't run for the rstudio user (dunno why).
+sudo rserver
+sleep 1
+sudo pkill rserver
+
+# Ensure git sees the directory as safe
+git config --global --add safe.directory /workspaces/$RepositoryName
